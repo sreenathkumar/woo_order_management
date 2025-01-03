b9f7ef0c18cad814272b85a8ff5f98abb6305f39
Sreenath Kumar <sreenathkumar66@gmail.com>
2025-01-03 16:36:28 +0600

    feat: added the search employee feature

--- app/(private)/employees/page.tsx
+++ app/(private)/employees/page.tsx
@@ -2,8 +2,19 @@ import { getAllEmployees } from "@/actions/employee"
 import EmployeeTableWrapper from "./components/EmployeeTableWrapper"
 
 
-async function EmployeesPage() {
-    const employees = await getAllEmployees();
+async function EmployeesPage({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
+    let employees = await getAllEmployees();
+
+    const { query } = await searchParams;
+
+    if (query) {
+        employees = employees.filter(employee => {
+            if (employee.name.toLowerCase().includes(query as string) || employee.role.toLowerCase().includes(query as string)) {
+                return employee
+            }
+        });
+
+    }
 
     return (
         <EmployeeTableWrapper employees={employees} />
--- components/ui/SearchField.tsx
+++ components/ui/SearchField.tsx
@@ -1,13 +1,32 @@
 'use client'
-import { useState } from 'react'
 import { Search } from 'lucide-react'
 
 import { Input } from "@/components/shadcn/input"
 import { cn } from '@/lib/utils'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 
 export default function SearchField({ className }: { className?: string }) {
-    const [searchQuery, setSearchQuery] = useState('')
+    //const [searchQuery, setSearchQuery] = useState('');
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        // create a new URLSearchParams object
+        const params = new URLSearchParams(searchParams);
+
+        if (e.target.value) {
+            params.set('query', e.target.value);
+        } else {
+            params.delete('query');
+        }
+        replace(`${pathname}?${params.toString()}`);
+        // update the search params in the URL
+        //setSearchQuery(e.target.value);
+    }
 
     return (
         <div className={cn(`flex items-center justify-between w-full ${className}`)}>
@@ -16,8 +35,8 @@ export default function SearchField({ className }: { className?: string }) {
                     type="search"
                     placeholder="Search..."
                     className="w-full pl-10 pr-4"
-                    value={searchQuery}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
+                    defaultValue={searchParams.get('query')?.toString()}
+                    onChange={handleSearch}
                 />
                 <Search size={20} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
             </form>
