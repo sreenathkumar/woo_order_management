1dcb65c35a44ee208a126775883b1c640fc7ed65
Sreenath Kumar <sreenathkumar66@gmail.com>
2025-01-15 17:28:27 +0600

    feat: increased the session time to 7days

--- auth.config.ts
+++ auth.config.ts
@@ -3,7 +3,7 @@ import { NextAuthConfig } from "next-auth";
 export const authConfig = {
     session: {
         strategy: "jwt",
-        maxAge: 24 * 60 * 60
+        maxAge: 7 * 24 * 60 * 60 // 7 days
     },
     providers: []
 } satisfies NextAuthConfig
