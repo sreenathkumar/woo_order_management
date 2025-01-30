7cca00c87ca51f55d378839cf77acd529bfbe909
Sreenath Kumar <sreenathkumar66@gmail.com>
2025-01-30 14:12:23 +0600

    feat: added the change of url on selecting the order filter type

--- app/(private)/orders/components/FilterBtn.tsx
+++ app/(private)/orders/components/FilterBtn.tsx
@@ -1,7 +1,22 @@
+'use client'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/shadcn/dropdown-menu"
 import { ListFilter } from "lucide-react"
+import { useRouter, useSearchParams } from "next/navigation";
+
+type SortType = 'default' | 'city_asc' | 'city_desc'
+
 
 function FilterBtn() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    //function to set the 
+    const handleSortFilter = (type: SortType) => {
+        const params = new URLSearchParams(searchParams.toString());
+        if (type === 'default') { params.delete('sort') } else { params.set('sort', type); }
+
+        router.push(`?${params.toString()}`);
+    }
     return (
         <DropdownMenu>
             <DropdownMenuTrigger className="flex items-center gap-2 border h-9 px-3 rounded-md">
@@ -9,8 +24,9 @@ function FilterBtn() {
                 Filters
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-                <DropdownMenuItem>City (Asc) </DropdownMenuItem>
-                <DropdownMenuItem>City (Desc)</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortFilter('default')}>Default</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortFilter('city_asc')}>City (Asc) </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortFilter('city_desc')}>City (Desc)</DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     )
