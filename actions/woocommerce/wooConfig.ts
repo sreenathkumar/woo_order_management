b0c5488611cbd1f4f61654aecfcf3a20829158c0
Sreenath Kumar <sreenathkumar66@gmail.com>
2025-02-22 19:34:29 +0600

    refactor: removed the code for extracting billing data from meta_data

--- actions/woocommerce/wooConfig.ts
+++ actions/woocommerce/wooConfig.ts
@@ -13,60 +13,26 @@ interface ExtendedOrderType extends OrderType {
     date_modified_gmt: string;
 }
 
-type MetaDataItem = {
-    id: number;
-    key: string;
-    value: string | object; // Adjust type based on possible values
-};
-
-type BillingData = {
-    _billing_block?: string;
-    _billing_street?: string;
-    _billing_house?: string;
-    _billing_jaddah?: string;
-    _billing_floor_apt?: string;
-    _billing_phone_2?: string;
-};
-
-// List of keys to extract
-const keysToExtract: (keyof BillingData)[] = [
-    "_billing_block",
-    "_billing_street",
-    "_billing_house",
-    "_billing_jaddah",
-    "_billing_floor_apt",
-    "_billing_phone_2",
-];
-
-
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const prepareOrder = (order: any) => {
     if (!order) return null;
 
-    const { id, total, billing: { first_name, state }, payment_method, date_created_gmt, date_modified_gmt, meta_data } = order;
-
-    // Extract the desired data
-    const billingData: BillingData = meta_data
-        .filter((item: MetaDataItem) => keysToExtract.includes(item.key as keyof BillingData))
-        .reduce((acc: BillingData, item: MetaDataItem) => {
-            acc[item.key as keyof BillingData] = item.value as string; // Type assertion to match BillingData
-            return acc;
-        }, {});
+    const { id, total, billing, payment_method, date_created_gmt, date_modified_gmt } = order;
 
     return {
         order_id: id || '',
         amount: total || '',
-        name: first_name || '',
-        city: state || '',
+        name: billing?.first_name || '',
+        city: billing?.state || '',
         address: {
-            block: billingData._billing_block || '',
-            street: billingData._billing_street || '',
-            house: billingData._billing_house || '',
-            jaddah: billingData._billing_jaddah || '',
-            floor_apt: billingData._billing_floor_apt || '',
+            block: billing?.billing_block || '',
+            street: billing?.billing_street || '',
+            house: billing?.billing_house || '',
+            jaddah: billing?.billing_jaddah || '',
+            floor_apt: billing?.billing_floor_apt || '',
         },
-        phone: billingData._billing_phone_2 || '',
+        phone: billing?.billing_phone_2 || '',
         payment: payment_method || '',
         date_created_gmt: date_created_gmt || '',
         date_modified_gmt: date_modified_gmt || '',
